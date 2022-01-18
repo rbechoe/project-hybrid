f3da2dc9d02ef0c206ea1770f37d8def77092e7b
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2022-01-18 12:15:32 +0100

    fixes to bossspawner

--- Assets/BossSpawner.cs
+++ Assets/BossSpawner.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 
 public class BossSpawner : MonoBehaviour
 {
+    private GameObject boss;
+    
     private void Start()
     {
-        gameObject.SetActive(false);
+        boss = FindObjectOfType<Boss>().gameObject;
+        boss.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
         EventSystem.AddListener(EventType.START_BOSS, StartBoss);
+    }
+
+    private void OnDisable()
+    {
+        EventSystem.RemoveListener(EventType.START_BOSS, StartBoss);
     }
 
     private void StartBoss()
     {
-        gameObject.SetActive(true);
+        boss.SetActive(true);
     }
 }
